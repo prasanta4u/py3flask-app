db06dbeb5e6c45ebf070f547ddea23c60b03d903
Prasanta Kumar Panda <prasanta.kumar@outlook.com>
2021-02-10 17:59:42 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+# list of cat images
+images = [
+    "https://images.unsplash.com/photo-1551705459-5611b0d0b60e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    "https://images.unsplash.com/flagged/photo-1571083720815-0e02060f1f84?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2122&q=80",
+    "https://images.unsplash.com/photo-1500614922032-b6dd337b1313?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+    ]
+
+@app.route('/')
+def index():
+    url = random.choice(images)
+    return render_template('index.html', url=url)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
+ 
+
